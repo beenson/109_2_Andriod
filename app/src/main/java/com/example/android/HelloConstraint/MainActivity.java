ca18c5e3795d0d670d3d30f04d29de4fd53472ee
labsesdt <labsesdt@gmail.com>
2021-03-02 17:31:16 +0800

    HW2

--- app/src/main/java/com/example/android/HelloConstraint/MainActivity.java
+++ app/src/main/java/com/example/android/HelloConstraint/MainActivity.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.example.android.hellotoast;
+package com.example.android.HelloConstraint;
 
 import android.graphics.Color;
 import android.os.Bundle;
